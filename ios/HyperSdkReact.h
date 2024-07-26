107997426cf4b3077d89dbf476db44914fdc423d
Hari Mohan <128347435+hmhdworks@users.noreply.github.com>
2024-07-26 14:22:27 +0530

    hypersdk

---
+++ ios/HyperSdkReact.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Juspay Technologies.
+ *
+ * This source code is licensed under the AGPL 3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventEmitter.h>
+#import <HyperSDK/HyperSDK.h>
+#import <React/RCTRootView.h>
+
+@interface HyperSdkReact : RCTEventEmitter <RCTBridgeModule>
+
+#import <HyperSDK/HyperSDK.h>
+@property HyperServices *hyperInstance;
+@property id <HyperDelegate> delegate;
+
+@end
+
+@interface SdkDelegate : NSObject <HyperDelegate>
+@property (nonatomic, strong) NSMutableDictionary *rootHolder;
+@property (nonatomic, strong) NSMutableDictionary *heightHolder;
+@property (nonatomic, strong) NSMutableDictionary *heightConstraintHolder;
+@property (nonatomic, strong) RCTBridge *bridge;
+- initWithBridge: (RCTBridge *) bridge;
+@end
+
+@interface SDKRootView : RCTRootView
+
+@property (nonatomic, strong) NSLayoutConstraint *leading;
+@property (nonatomic, strong) NSLayoutConstraint *trailing;
+
+@end
